5188d4c179177b426cfd69fd1802aca7a6503f8a
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-02 12:03:11 +0200

    test first user creation in db

---
+++ server/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+dotenv.config();
+const app = express();
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
+
+app.options("*", cors());
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+/*
+async function main() {
+    const user = await prisma.user.create({
+      data: {
+        username: "andrea",
+        password: "password",
+        name: "Andrea"
+      },
+    })
+
+    console.log(user);
+}
+
+main()
+.catch(e => {
+    console.error(e.message)
+})
+.finally(async () => {
+    await prisma.$disconnect()
+})
+*/
+app.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(`Server listening on localhost:${PORT}`);
+  });
