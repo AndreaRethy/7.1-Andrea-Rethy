a920fe17577f799fe6ff74e858c18d0df7db9b84
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-02 13:57:02 +0200

    Add put and delete user routes

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -13,7 +13,14 @@ router.get('/users/:id', async (req, res) => {
         where: {
             id: parseInt(req.params.id),
         },
+       /* include: {
+            publications: true,
+            liked: true
+        } */
     });
+    if (!foundUser) {
+        return res.status(404).json({ error: "User not found" })
+    }
     return res.json(foundUser);
 });
 
@@ -22,6 +29,31 @@ router.post('/users', async (req, res) => {
         data: req.body,
     });
     res.json(newUser);
+});
+
+router.put('/users/:id', async (req, res) => {
+    const editedUser = await prisma.user.update({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        data: req.body
+    });
+    if (!editedUser) {
+        return res.status(404).json({ error: "User not found" })
+    }
+    return res.json(editedUser);
+});
+
+router.delete('/users/:id', async (req, res) => {
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: parseInt(req.params.id),
+        },
+    });
+    if (!deletedUser) {
+        return res.status(404).json({ error: "User not found" })
+    }
+    return res.json(deletedUser);
 });
 
 export default router;
