41f82ce24d634c8341dc153c6996048ea9776fd9
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-03 13:07:41 +0200

    Create domain / UserRepository

---
+++ server/src/domain/repositories/UserRepository.ts
@@ -0,0 +1,9 @@
+import { User } from '../entities/User.js';
+
+export interface UserRepository {
+    getAllUsers(): Promise<User[]>;
+    getUserById(id: Number): Promise<User | null>;
+    getUserLogin(username: String, password: String): Promise<User | null>;
+    createUser(user: User): Promise<User>;
+    updateUser(id: Number): Promise<void>;
+}
