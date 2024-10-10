2f818145f7e2d8369cc80e3ae4851b7ae1cfc6bf
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-10 10:50:40 +0200

    Create publication repository implementation methods, update publicationRepository

--- server/src/domain/repositories/PublicationRepository.ts
+++ server/src/domain/repositories/PublicationRepository.ts
@@ -1,10 +1,10 @@
 import { Publication } from "../entities/Publication.js";
 
 export interface publicationRepository {
-    getPublicationsForUser(userId: Number): Promise<Publication[]>;
+    getPublicationsForUser(username: String): Promise<Publication[]>;
     createPublication(publication: Publication): Promise<Publication>;
-    likePublication(publicationId: Number): Promise<Publication>;
-    updatePublication(publicationId: Number): Promise<Publication>;
-    deletePublication(publicationId: Number): Promise<void>;
-    recoverPublication(publicationId: Number): Promise<void>;
+    likePublication(publicationId: Number): Promise<Publication | null>;
+    updatePublication(publicationId: Number): Promise<Publication | null>;
+    deletePublication(publicationId: Number): Promise<Publication | null>;
+    recoverPublication(publicationId: Number): Promise<Publication | null>;
 }
---
+++ server/src/infrastructure/repositories/publicationRepositoryImpl.ts
@@ -0,0 +1,80 @@
+import { prisma } from '../../db.js';
+import { Publication } from "@prisma/client";
+
+export class publicationRepositoryImpl {
+    async getPublicationsForUser(username: string): Promise<Publication[]> {
+        return await prisma.publication.findMany({
+            where: {
+              author: {
+                username: username,
+              },
+            },
+          });
+    }
+
+    async createPublication(data: 
+        Omit<Publication, "id" | "createdAt" | "updatedAt">
+    ): Promise<Publication> {
+        return await prisma.publication.create({
+            data: {
+                ...data,
+            },
+        });
+    }
+
+    async likePublication(
+        id: number,
+        data: Partial<Omit<Publication, "id">>
+    ): Promise<Publication | null> {
+        try {
+            return await prisma.publication.update({
+                where: { id },
+                data,
+              });
+        } catch (error) {
+            throw new Error(`Publication not found or error updating publication: ${error}`);
+          }
+    }
+
+    async updatePublication(
+        id: number,
+        data: Partial<Omit<Publication, "id">>
+    ): Promise<Publication | null> {
+        try {
+            return await prisma.publication.update({
+                where: { id },
+                data,
+              });
+        } catch (error) {
+            throw new Error(`Publication not found or error updating publication: ${error}`);
+          }
+    }
+
+    async deletePublication(
+        id: number,
+        data: Partial<Omit<Publication, "id">>
+    ): Promise<Publication | null> {
+        try {
+            return await prisma.publication.update({
+                where: { id },
+                data,
+              });
+        } catch (error) {
+            throw new Error(`Publication not found or error updating publication: ${error}`);
+          }
+    }
+
+    async recoverPublication(
+        id: number,
+        data: Partial<Omit<Publication, "id">>
+    ): Promise<Publication | null> {
+        try {
+            return await prisma.publication.update({
+                where: { id },
+                data,
+              });
+        } catch (error) {
+            throw new Error(`Publication not found or error updating publication: ${error}`);
+          }
+    }
+}
