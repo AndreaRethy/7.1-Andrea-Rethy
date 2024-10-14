3f4ea7f084b9fb128aba2dd8f1f7d2df189af915
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-14 10:28:56 +0200

    Add routes for publications

---
+++ server/src/routes/publications.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { PublicationController } from "src/infrastructure/controllers/publicationController.js";
+
+const router = Router();
+const publicationController = new PublicationController();
+
+router.get("/publications", publicationController.getAllPublications);
+router.get("/publications/:username", publicationController.getPublicationsForUser);
+router.get("/publications/:id", publicationController.getPublicationById);
+router.post("/publications", publicationController.createPublication);
+router.put("publications/:id", publicationController.updatePublication);
+router.patch("/publications/:id/like", publicationController.likePublication);
+router.patch("/publications/:id/delete", publicationController.deletePublication);
+router.patch("/publications/:id/restore", publicationController.restorePublication);
