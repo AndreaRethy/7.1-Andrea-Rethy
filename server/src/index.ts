86c052fde12a21e433f80fbbde5504de15cbd51f
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-17 12:18:50 +0200

    Move admin routes to users routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import loginRoutes from './routes/login.routes.js';
 import userRoutes from './routes/users.routes.js'
 import publicationRoutes from './routes/publications.routes.js'
-import adminRoutes from './routes/admin.routes.js'
+//import adminRoutes from './routes/admin.routes.js'
 import swaggerDocs from './routes/swagger.js'
 
 dotenv.config();
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use('/api/v1', loginRoutes);
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', publicationRoutes);
-app.use('/api/v1', adminRoutes);
+//app.use('/api/v1', adminRoutes);
 
 app.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server listening on localhost:${PORT}`);
--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -2,9 +2,40 @@ import { Router } from 'express';
 import { UserController } from '../infrastructure/controllers/userController.js';
 
 import authMiddlewareJWT from "../middleware/authMiddleware.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const router = Router();
 const userController = new UserController();
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *       401:
+ *         description: Unauthorized
+ */
+router.get("/users", adminAuth, userController.getAllUsers);
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   patch:
+ *     summary: Update user role and/or ban status
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *       401:
+ *         description: Unauthorized
+ */
+router.patch("/users/:id", adminAuth, userController.updateUserByAdmin);
 
 /**
  * @swagger
@@ -16,6 +47,8 @@ const userController = new UserController();
  *     responses:
  *       200:
  *         description: A list of users.
+ *       401:
+ *         description: Unauthorized
  */
 router.get("/users/:id", authMiddlewareJWT, userController.getUserById);
 
@@ -29,6 +62,8 @@ router.get("/users/:id", authMiddlewareJWT, userController.getUserById);
  *     responses:
  *       200:
  *         description: A list of users.
+ *       401:
+ *         description: Unauthorized
  */
 router.put("/users/:id", authMiddlewareJWT, userController.updateUser);
 
