452605e91a849c391ccf6bda757f3a6f5af9fb7c
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-30 11:16:20 +0100

    Add view for listing all existing users

---
+++ client/src/components/Users.tsx
@@ -0,0 +1,85 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const URL = "/api/v1/users";
+
+export type User = {
+  id: number;
+  username: string;
+  name: string;
+  role: "USER" | "ADMIN";
+  isBanned: boolean;
+}
+
+const Users = () => {
+  const navigate = useNavigate();
+  const [ users, setUsers ] = useState<User[]>([])
+  const [token, setToken] = useState<string | null>(null);
+
+  const onWindowLoad = () => {
+    getUsers();
+  };
+
+  useEffect(() => {
+    const storedToken = sessionStorage.getItem('token');
+
+    if (storedToken) {
+      setToken(storedToken);
+    } else {
+      navigate('/');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token !== null) {
+      onWindowLoad();
+    }
+  }, [token]);
+
+  function getUsers() {
+    fetch(`${URL}`, {
+      method: 'GET',
+      headers: { 
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          if (response.status === 403 && errorData.error === "Invalid token") {
+            navigate("/");
+          } else {
+            throw new Error(errorData.error || 'An error occured');
+          }
+        });
+      }
+      return response.json();
+    })
+      .then((data) => {
+        setUsers(data)
+      } 
+    )
+      .catch((error) => console.error('Error:', error));
+  }
+
+  return (
+    <div className="p-10 flex flex-col justify-start align-top items-start">
+      <h2 className="text-slate-800 text-2xl font-bold pb-4">List of All Users</h2>
+      <ul className="flex flex-col justify-start align-top items-start">
+      <li key={0} className="text-slate-800 font-semibold pb-2">ID - Name - Username - Role</li>
+        {
+          users.map((user, index) => {
+            return (
+              <li key={index + 1} className="text-slate-800">{user.id} - {user.name} - {user.username} - {user.role}</li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Users;
