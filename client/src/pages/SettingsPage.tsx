a4fdb42cba5fc6da4f16e7aec0a6f83ef5443cad
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-10-30 12:50:18 +0100

    Update SettingsPage

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import background from '../assets/background.jpg';
 import { FaUser, FaLock, FaUserTag } from "react-icons/fa";
 import { FaClipboardUser, FaUserGear, FaUserLargeSlash, FaArrowLeftLong } from "react-icons/fa6";
@@ -9,7 +10,7 @@ const URL = "/api/v1/users/"
 const ProfilePage = () => {
   const navigate = useNavigate();
   const [token, setToken] = useState<string | null>(null);
-  const [userId, setUserId] = useState<number>(0);
+  const [userId, setUserId] = useState<string>("");
   const [role, setRole] = useState<"USER" | "ADMIN">("USER");
   const [isBanned, setIsBanned] = useState<boolean>(false);
   const [myUserId, setMyUserId] = useState<number | null>(null);
@@ -22,7 +23,42 @@ const ProfilePage = () => {
     setMyUserId(Number(sessionStorage.getItem("userId")));
   }, [token, myUserId]);
 
-  function updateUser() {
+  useEffect(() => {
+    if (myUserId != null) {
+      getUser()
+    }
+  })
+
+  function getUser() {
+    fetch(`${URL}${myUserId}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          if (response.status === 403 && errorData.error === "Invalid token") {
+            navigate("/");
+          } else {
+            throw new Error(errorData.error || 'An error occured');
+          }
+        });
+      }
+      return response.json();
+    })
+      .then((data) => {
+        setName(data.name)
+        setUsername(data.username)
+      } 
+    )
+      .catch((error) => console.error('Error:', error));
+  }
+
+  function updateProfile() {
     fetch(`${URL}${myUserId}`, {
       method: 'PUT',
       headers: {
@@ -62,13 +98,56 @@ const ProfilePage = () => {
       .catch((error) => console.error('Error:', error));
   }
   
-  const handleUpdate = (e: React.FormEvent) => {
+  const handleProfileUpdate = (e: React.FormEvent) => {
     
     e.preventDefault();
     if (username !== null && username.trim()) {
-      updateUser();
+      updateProfile();
     } else {
       alert("Username cannot be empty");
+    }
+  };
+
+  function updateUser() {
+    fetch(`${URL}${userId}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ 
+        role: role,
+        isBanned: isBanned,
+       }),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          if (response.status === 403 && errorData.error === "Invalid token") {
+            navigate("/");
+          } else {
+            throw new Error(errorData.error || 'An error occured');
+          }
+        });
+      }
+      return response.json();
+    })
+      .then((data) => {
+        console.log(data)
+          navigate("/home");
+      } 
+    )
+      .catch((error) => console.error('Error:', error));
+  }
+  
+  const handleUserUpdate = (e: React.FormEvent) => {
+    
+    e.preventDefault();
+    if (userId !== "" && userId.trim()) {
+      updateUser();
+    } else {
+      alert("UserID has to be set");
     }
   };
 
@@ -76,56 +155,56 @@ const ProfilePage = () => {
     navigate("/home");
   }
 
+  const handleSignOut = () => {
+    // TODO: Empty sessionStorage
+    navigate("/");
+  }
+
   return (
     <section className='p-4 h-screen bg-black' style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <FaArrowLeftLong className='text-slate-900 text-4xl z-10' onClick={goBack}/>
-      <div className='flex justify-center items-center h-full z-0' >
-        <div className='m-4 p-20 rounded-xl backdrop-blur-lg shadow-md shadow-slate-500'>
-          <form onSubmit={handleUpdate}>
-            <h2 className='text-slate-900 text-5xl font-semibold my-4'>Update Profile</h2>
+      <div className='flex flex-col h-full'>
+        <FaArrowLeftLong className='text-slate-900 text-4xl z-10' onClick={goBack}/>
+        <div className='flex justify-center items-center h-full z-0' >
+          <div className='m-4 p-20 rounded-xl backdrop-blur-lg shadow-md shadow-slate-500'>
+            <form onSubmit={handleProfileUpdate}>
+              <h2 className='text-slate-900 text-5xl font-semibold my-4'>Update Profile</h2>
 
-            {/* Name */}
-            <div className='relative'>
-              <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='text' placeholder='Name' required value={name} id='name' onChange={(e) => setName(e.target.value)} />
-              <FaUser className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
-            </div>
+              {/* Name */}
+              <div className='relative'>
+                <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='text' placeholder='Name' required value={name} id='name' onChange={(e) => setName(e.target.value)} />
+                <FaUser className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
+              </div>
 
-            {/* Username */}
-            <div className='relative'>
-              <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='text' placeholder='Username' required value={username} id='username' onChange={(e) => setUsername(e.target.value)} />
-              <FaUserTag className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
-            </div>
+              {/* Username */}
+              <div className='relative'>
+                <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='text' placeholder='Username' required value={username} id='username' onChange={(e) => setUsername(e.target.value)} />
+                <FaUserTag className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
+              </div>
 
-            {/* Password */}
-            <div className='relative'>
-              <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='password' placeholder='Password' required value={password} id='username' onChange={(e) => setPassword(e.target.value)} />
-              <FaLock className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4'/>
-            </div>
-            <button type='submit' className='py-3 px-6 m-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-900 font-semibold hover:opacity-85'>Submit</button>
-          </form>
-        </div>
+              {/* Password */}
+              <div className='relative'>
+                <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='password' placeholder='Password' required value={password} id='username' onChange={(e) => setPassword(e.target.value)} />
+                <FaLock className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4'/>
+              </div>
+              <button type='submit' className='py-3 px-6 m-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-900 font-semibold hover:opacity-85'>Submit</button>
+            </form>
+          </div>
 
-        {/* TODO: Should be only visible to admins! */}
-        <div className='m-4 p-20 rounded-xl backdrop-blur-lg shadow-md shadow-slate-500'>
-          <form onSubmit={handleUpdate}>
-            <h2 className='text-slate-900 text-5xl font-semibold my-4'>Update User</h2>
+          {/* TODO: Should be only visible to admins! */}
+          <div className='m-4 p-20 rounded-xl backdrop-blur-lg shadow-md shadow-slate-500'>
+            <form onSubmit={handleUserUpdate}>
+              <h2 className='text-slate-900 text-5xl font-semibold my-4'>Update User</h2>
 
-            {/* ID */}
-            <div className='relative'>
-              <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='text' placeholder='User ID' required value={userId} id='name' onChange={(e) => setUserId(parseInt(e.target.value))} />
-              <FaClipboardUser className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
-            </div>
-            <div>
+              {/* ID */}
+              <div className='relative'>
+                <input className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none' type='string' placeholder='User ID' required value={userId} id='userId' onChange={(e) => setUserId(e.target.value)} />
+                <FaClipboardUser className='text-slate-900 absolute right-8 top-1/2 -translate-y-2/4' />
+              </div>
 
               {/* Role Dropdown */}
               <div className='relative'>
                 <select
-                  className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none min-w-full'
-                  value={role}
-                  id='role'
-                  onChange={(e) => setRole(e.target.value as "USER" | "ADMIN")}
-                  required
-                >
+                  className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none min-w-full' value={role} id='role' onChange={(e) => setRole(e.target.value as "USER" | "ADMIN")} required >
                   <option value="USER">User</option>
                   <option value="ADMIN">Admin</option>
                 </select>
@@ -135,21 +214,18 @@ const ProfilePage = () => {
               {/* Is Banned Dropdown */}
               <div className='relative'>
                 <select
-                  className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none min-w-full'
-                  value={isBanned ? "true" : "false"}
-                  id='isBanned'
-                  onChange={(e) => setIsBanned(e.target.value === "true")}
-                  required
-                >
+                  className='rounded-md p-3 my-3 border border-slate-900 text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 transition duration-200 bg-slate-50 opacity-70 appearance-none min-w-full' value={isBanned ? "true" : "false"} id='isBanned' onChange={(e) => setIsBanned(e.target.value === "true")} required >
                   <option value="false">No</option>
                   <option value="true">Yes</option>
                 </select>
                 <FaUserLargeSlash className='text-slate-900 absolute right-8 top-1/2 transform -translate-y-2/4' />
               </div>
-            </div>
-            <button type='submit' className='py-3 px-6 m-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-900 font-semibold hover:opacity-85'>Submit</button>
-          </form>
+              <button type='submit' className='py-3 px-6 m-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-900 font-semibold hover:opacity-85'>Submit</button>
+            </form>
+          </div>
+          
         </div>
+        <button className='rounded-lg text-xl font-semibold p-4 bg-slate-50 m-auto w-max' onClick={handleSignOut}>Sign Out</button>
       </div>
     </section>
   )
